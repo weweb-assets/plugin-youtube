dc9332b533f6020319d5fd962d76cc3a0e33c422
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:33:28 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -1,5 +1,20 @@
 export default {
     editor: {
         designSystemId: 'ab8d890a-ac3a-4678-9e80-e0c91005a495',
+        /* wwEditor:start */
+        settings: {
+            copilot: {
+                description: 'Configure YouTube plugin settings',
+                returns: 'void',
+                schema: {
+                    apiKey: {
+                        type: 'string',
+                        description: 'YouTube API key for accessing YouTube services',
+                        bindable: false
+                    }
+                }
+            }
+        }
+        /* wwEditor:end */
     },
 };
